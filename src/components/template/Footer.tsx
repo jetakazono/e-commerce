830f63c5f72d985c39dcad29831870c1b25e8601
jetakazono <jessica.takazono@gmail.com>
2024-07-31 17:16:21 +0100

    fix component syle

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="flex justify-end items-center bg-zinc-800 h-10 px-10">
+        <footer className="flex justify-center lg:justify-end items-center bg-zinc-800 h-10 px-10">
             <p className="text-xs">Made by Jessie with 🧡</p>
         </footer>
     )
