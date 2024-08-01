372aac9878cdbcf1b6bc7dcc4ea4741e09e2d388
jetakazono <jessica.takazono@gmail.com>
2024-08-01 21:18:29 +0100

    fix header

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 
 export default function Header() {
     return (
-        <header className="flex justify-between items-center bg-zinc-800 h-20 px-10">
+        <header className="flex justify-between items-center bg-zinc-800 h-20 px-10 sticky w-full top-0 z-10">
             <Logo />
             <Basket />
         </header>
